8cf63248bdcd026fa6e966a8d0888b97dd35ac58
Jay Patel <jp3592@drexel,edu>
2020-03-08 14:05:40 -0400

    Changes to Location file

--- Location.py
+++ Location.py
@@ -1,6 +1,9 @@
 # Assembly of the models at the factory specified
+from Model import Model
+
+
 class Location:
-    def __init__(self, location = None, model = None):
+    def __init__(self, location=None, model=None):
         self.location = location
         self.model = model
 
@@ -27,4 +30,58 @@ class Location:
 
     def __str__(self):
         # Return info about the models and location
-        return "Factory Location: %s\nLabel: %s\nPrice: %s\n" %(self.location, self.model.label, self.model.price)
+        return "Factory Location: %s\nLabel: %s\nPrice: %s\n" % (self.location, self.model.label, self.model.price)
+
+
+class Singapore():
+    def __init__(self):
+        Singapore = Location("Singapore")
+
+        Smart4 = Model()
+        Smart5 = Model()
+
+        # Smart4 specifications
+        Smart4.setLabel("Smart4")
+        Smart4.setPrice("$400")
+
+        # Smart5 specifications
+        Smart5.setLabel("Smart5")
+        Smart5.setPrice("$599.99")
+
+        # Assembly of Smart4 and Smart5 in Singapore
+        Singapore4 = Singapore.assembleSmart(Smart4)
+        # Print the location of the Singapore Factory
+        print(Singapore)
+
+        Singapore5 = Singapore.assembleSmart(Smart5)
+        print(Singapore)
+
+
+class NewYork():
+    def __init__(self):
+        NewYork = Location("New York")
+
+        Smart5 = Model()
+        Smart6 = Model()
+
+        # Smart5 specifications
+        Smart5.setLabel("Smart5")
+        Smart5.setPrice("$599.99")
+
+        # Smart6 specifications
+        Smart6.setLabel("Smart6")
+        Smart6.setPrice("$1099.99")
+
+        # Assembly of Smart5 and Smart6 in New York
+        NewYork5 = NewYork.assembleSmart(Smart5)
+        # Print the location of the New York Factory
+        print(NewYork)
+
+        NewYork6 = NewYork.assembleSmart(Smart6)
+        print(NewYork)
+
+
+if __name__ == "__main__":
+    # Test to see if it works
+    Singapore()
+    NewYork()
